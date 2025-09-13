ed2256006e9252987682df07eaacee3874b28fc8
Justin <justinr32274@gmail.com>
2025-09-13 22:18:58 +0530

    Created the Frame

--- FreelanceHub/src/Freelance/Main.java
+++ FreelanceHub/src/Freelance/Main.java
@@ -1,9 +1,17 @@
 package Freelance;
 
+import java.awt.*; 
+import javax.swing.*; 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JFrame frame=new JFrame("FreelanceHub");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width=screenSize.width;
+		int height=screenSize.height;
+		frame.setSize(width,height);
+		frame.setVisible(true);
 
 	}
 
