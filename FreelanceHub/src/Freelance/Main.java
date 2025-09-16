72876d88c3831a974900895fc029842873e8b540
Justin <justinr32274@gmail.com>
2025-09-17 00:05:32 +0530

    Added the buttons and adjusted the box layout

--- FreelanceHub/src/Freelance/Main.java
+++ FreelanceHub/src/Freelance/Main.java
@@ -2,6 +2,7 @@ package Freelance;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,16 +26,30 @@ public class Main {
                 + "along with database connectivity.<br><br>"
                 + "Though not a commercial product, <strong>FreelanceHub</strong> serves as a<br>"
                 + "learning project that demonstrates how logic can be applied<br>"
-                + "to real-world scenarios."
+                + "to real-world scenarios.<br><br>"
                 + "</div></html>");
         text.setEditable(false);
         text.setBackground(Color.LIGHT_GRAY);
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBackground(Color.LIGHT_GRAY);
-
+        JButton button=new JButton("Login !!");
+        JButton button2=new JButton("Register!!");
+        button.setBackground(Color.CYAN);   
+        button.setOpaque(true);             
+        button.setBorderPainted(false);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setMaximumSize(new Dimension(500, 50));
+        button.setFont(new Font ("Times New Roman",Font.BOLD,20));
+        button2.setMaximumSize(new Dimension(500, 50));
+        button2.setFont(new Font ("Times New Roman",Font.BOLD,20));
+        button2.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(label1);
+        panel.add(button);
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(button2);
         panel.add(text);
+        
 
         JScrollPane scroll = new JScrollPane(panel);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
