f498b228e39ca80b0555d5bb8b5b0e91652629d7
Justin <justinr32274@gmail.com>
2025-09-17 22:53:04 +0530

    Updated the logo and fixed the spacing issue.

---
+++ FreelanceHub/src/Freelance/Auth.java
@@ -0,0 +1,8 @@
+package Freelance;
+
+public class Auth {
+	
+	
+	
+
+}
--- FreelanceHub/src/Freelance/Main.java
+++ FreelanceHub/src/Freelance/Main.java
@@ -1,8 +1,8 @@
 package Freelance;
-
+import java.util.*;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.Border;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,22 +11,23 @@ public class Main {
         int width = screenSize.width;
         int height = screenSize.height;
         frame.setSize(width, height);
-
         frame.getContentPane().setBackground(Color.LIGHT_GRAY);
-
-        ImageIcon icon = new ImageIcon(Main.class.getResource("/images/freelance-removebg-preview.png"));
+        ImageIcon icon = new ImageIcon(Main.class.getResource("/images/freelance2-Photoroom.png"));
         JLabel label1 = new JLabel(icon);
+        label1.setBackground(Color.LIGHT_GRAY);
         label1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        label1.setSize(200,200);
+
         JTextPane text = new JTextPane();
         text.setContentType("text/html");
-        text.setText("<html><div style='text-align:center;font-family:Times New Roman;font-size:34'>"
-                + "<strong> FreelanceHub </strong> is a Java-based project built to explore how<br>"
+        text.setText("<html><div style='text-align:center;font-family:Times New Roman;font-size:24'>"
+                + "<strong> FreelanceHub </strong> is a Java-based project built to explore how"
                 + "projects can be organized and managed through software.<br>"
-                + "It focuses on basics of object-oriented programming concepts<br>"
+                + "It focuses on basics of object-oriented programming concepts"
                 + "along with database connectivity.<br><br>"
                 + "Though not a commercial product, <strong>FreelanceHub</strong> serves as a<br>"
                 + "learning project that demonstrates how logic can be applied<br>"
-                + "to real-world scenarios.<br><br>"
+                + "to real-world scenarios."
                 + "</div></html>");
         text.setEditable(false);
         text.setBackground(Color.LIGHT_GRAY);
@@ -36,19 +37,20 @@ public class Main {
         JButton button=new JButton("Login !!");
         JButton button2=new JButton("Register!!");
         button.setBackground(Color.CYAN);   
-        button.setOpaque(true);             
-        button.setBorderPainted(false);
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         button.setMaximumSize(new Dimension(500, 50));
         button.setFont(new Font ("Times New Roman",Font.BOLD,20));
         button2.setMaximumSize(new Dimension(500, 50));
         button2.setFont(new Font ("Times New Roman",Font.BOLD,20));
         button2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(label1);
-        panel.add(button);
+        panel.add(Box.createVerticalStrut(5));
+        panel.add(label1);  
         panel.add(Box.createVerticalStrut(20));
+        panel.add(button);
+        panel.add(Box.createVerticalStrut(10));
         panel.add(button2);
         panel.add(text);
+        
         
 
         JScrollPane scroll = new JScrollPane(panel);
